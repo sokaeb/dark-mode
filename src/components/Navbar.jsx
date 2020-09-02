77a03c135affec3496ae13aaf3e925692ac3004e
Kae Benton <sonja.k.benton@gmail.com>
2020-09-02 15:34:47 -0400

    Working on useDarkMode hook.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useDarkMode } from '../hooks/useDarkMode';
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  // const [darkMode, setDarkMode] = useState(false);
+  const [value, setValue] = useDarkMode();
   const toggleMode = e => {
     e.preventDefault();
-    setDarkMode(!darkMode);
+    setValue(!value);
   };
   return (
     <nav className="navbar">
@@ -12,7 +14,7 @@ const Navbar = () => {
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
+          className={'dark-mode' ? 'toggle toggled' : 'toggle'}
         />
       </div>
     </nav>
